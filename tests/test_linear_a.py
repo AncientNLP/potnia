146f77969dd94d5faf3f59b991eea385f6bdbefd
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-03-25 15:52:06 +1100

    :cop: changing tokenization tests to yaml format

--- tests/test_linear_a.py
+++ tests/test_linear_a.py
@@ -13,13 +13,8 @@ def test_linear_a_transliteration(test_input, expected):
     assert linear_a_mapper.to_transliteration(test_input) == expected
 
 
-def test_tokenize_transliteration_linear_a():
-    result = linear_a_mapper.tokenize_transliteration("]ta-pi ]ki[ ]a-ra[ ]a-su-mi-*118[ a-pa-[?][ ]mi-ki-sa-ne[")
-    assert result == [
-        "]", "ta", "pi", " ", 
-        "]", "ki", "[", " ", 
-        "]", "a", "ra", "[", " ", 
-        "]", "a", "su", "mi", "*118", "[", " ", 
-        "a", "pa", "[?]", "[", " ", 
-        "]", "mi", "ki", "sa", "ne", "[",
-    ]
+@pytest.mark.parametrize("test_input,expected", expected("linear_a_tokenize_transliteration"))
+def test_tokenize_transliteration_linear_a(test_input, expected):
+    assert linear_a_mapper.tokenize_transliteration(test_input) == expected
+
+
