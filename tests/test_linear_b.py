08cbfb806a2f6d8ac166f641fca8408812668cad
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-03-26 17:02:08 +1100

    :zap: adding quotes to yaml files

--- tests/test_linear_b.py
+++ tests/test_linear_b.py
@@ -15,6 +15,4 @@ def test_linear_b_transliteration(test_input, expected):
 
 @pytest.mark.parametrize("test_input,expected", expected("linear_b_tokenize_transliteration"))
 def test_tokenize_transliteration_linear_b(test_input, expected):
-    print("test_input", test_input)
-    print("expected", expected)
     assert linear_b_mapper.tokenize_transliteration(test_input) == expected
