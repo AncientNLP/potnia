4a1228246133cdbd8a2451042d48ef1b9d6b543a
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-03-26 11:45:10 +1100

    :zap: adding Kabirs tests

--- tests/test_linear_b.py
+++ tests/test_linear_b.py
@@ -15,5 +15,6 @@ def test_linear_b_transliteration(test_input, expected):
 
 @pytest.mark.parametrize("test_input,expected", expected("linear_b_tokenize_transliteration"))
 def test_tokenize_transliteration_linear_b(test_input, expected):
-    breakpoint()
+    print("test_input", test_input)
+    print("expected", expected)
     assert linear_b_mapper.tokenize_transliteration(test_input) == expected
