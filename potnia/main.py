71d96685f4596975d70f3789994f12b91a923be7
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-10-19 17:13:46 +1100

    :cop: adding tests for main

--- potnia/main.py
+++ potnia/main.py
@@ -7,13 +7,12 @@ app = typer.Typer()
 
 @app.command()
 def linear_a(text: str, regularize:bool=False):
+    """ Converts a Linear A text to Unicode. """
     print(linear_a_mapper(text, regularize=regularize))
 
 
 @app.command()
 def linear_b(text: str, regularize:bool=False):
+    """ Converts a Linear B text to Unicode. """
     print(linear_b_mapper(text, regularize=regularize))
 
-
-if __name__ == "__main__":
-    app()
---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import pytest
+
+from typer.testing import CliRunner
+from potnia.main import app
+from .data import expected
+
+runner = CliRunner()
+
+
+@pytest.mark.parametrize("test_input,expected", expected("linear_a_unicode"))
+def test_linear_a_main(test_input, expected):
+    result = runner.invoke(app, ["linear-a", test_input])
+    assert expected in result.stdout
+
+
+@pytest.mark.parametrize("test_input,expected", expected("linear_b_unicode"))
+def test_linear_b_main(test_input, expected):
+    result = runner.invoke(app, ["linear-b", test_input])
+    assert expected in result.stdout
+
+
+@pytest.mark.parametrize("test_input,expected", expected("linear_b_unicode_regularized"))
+def test_linear_b_main_regularized(test_input, expected):
+    result = runner.invoke(app, ["linear-b", test_input, "--regularize"])
+    assert expected in result.stdout
+
+
+
