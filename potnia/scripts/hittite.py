128e77ff0aca98be64ad9250045f6d7a1b01fa1d
Chris <chrisguest@gmail.com>
2025-11-20 00:45:19 +1100

    :zap: add remove_epigraphic_annotations preprocessing method

--- potnia/scripts/hittite.py
+++ potnia/scripts/hittite.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from ..script import Script
 import unicodedata
+import re
 
 
 @dataclass
@@ -59,6 +60,48 @@ class Hittite(Script):
 
         return tokens
 
+    def remove_epigraphic_annotations(self, text):
+        """
+        Removes some epigraphic annotations that has appeared in the text portion of a few hundred documents in HPM.
+        """
+        # this kind of parsing probably belongs in Ancient Corpora.
+        text = re.sub(r"obv \d+′ ", "", text)
+        text = re.sub(r'Frg( \d)+( [VR]s)( [IVX]+)?( \d+)′?', '', text)
+        text = re.sub('Ende [VR]s[.]? ?[IVX]*', '', text)
+        text = re.sub('[VR]s[.]?[?!]?( lk.)?( Kol.)? ?[IVX/]*[?]* bricht ab', '', text)
+        text = re.sub('ca [0-9-]+ (Zeichen|segni)', '', text)
+
+        epigraphic_annotations = [
+            'ca. 15 unbeschriebene Zeilenenden Rs. IV bricht ab',
+            'ca. 3 Zeilenanfänge unbeschrieben r. Kol. des lk. Rd. bricht ab',
+            'ca. 4 unbeschriebene Zeilenanfänge Rs. lk. Kol. bricht ab',
+            'ca. 6 Zeilen unbeschrieben // Tafelende Rs. IV bricht ab',
+            'erhaltener Teil einer Zeile unbeschrieben Rs. VI bricht ab',
+            'erhaltener Teil von ca. 2 Zeilen unbeschrieben Rs. IV bricht ab',
+            'erhaltener Teil von ca. 7 Zeilen unbeschrieben Rs. bricht ab',
+            'erhaltener Teil von ca 17 Zeilen unbeschrieben Tafelende',
+            'erhaltener Teil von ca. 17 Zeilen unbeschrieben Tafelende',
+            'unbeschriebene Zeilen, dann bricht der Text ab',
+            'nach ca. 2 unbeschriebene Zeilen bricht die Tafel ab',
+            'Rs. VI? bricht ca. 3 Zeilen vor Kolumnenende ab',
+            'Text bricht ab',
+            'lk. Kol. bricht ab',
+            'lk. Rd. bricht ab',
+            '\" lg=\"Hit\"/>',
+            'Kolophon',
+            ' "/>',
+            'breaks off',
+            # other ca formulations
+            'ca'
+        ]
+        for annotation in epigraphic_annotations:
+            text = text.replace(annotation, '')
+
+        # túḫ<span class="MaterLect">uḫ</span>-ša should be parsed as túḫ.uḫ-ša
+        text = text.replace('túḫuḫ', 'túḫ.uḫ')
+
+        return text
+
     def to_unicode(self, text: str, regularize: bool = False) -> str:
         """
         Converts transliterated text to unicode format with additional handling for Hittite-specific cases.
@@ -68,8 +111,10 @@ class Hittite(Script):
         text = text.replace('〈', '').replace('〉', '').replace('〈', '').replace('〉', '')
         text = text.replace('?', '').replace('*', '').strip()
 
+        text = self.remove_epigraphic_annotations(text)
 
         tokens = self.tokenize_transliteration(text)
+
         tokens = [
             token if ("MEŠ" in token or "meš" in token)
             else token.replace("(", "").replace(")", "")
